03a1cee5729ff2d07477b79d561279e387af7a8c
qq <qq@qq.com>
2024-08-08 16:49:43 +0800

    11 Signed-off-by: qq <qq@qq.com>

--- build/DIY/diy-part2.sh
+++ build/DIY/diy-part2.sh
@@ -26,14 +26,14 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 # ttyd 自动登录
 sed -i "s?/bin/login?/usr/libexec/login.sh?g" package/feeds/packages/ttyd/files/ttyd.config
 
-# 修改主机名（不能纯数字或者中文）
-# sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate                                                                    ## 把初始默认的“OpenWrt” 修改为：G-DOCK
-# sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrtx86'' package/lean/default-settings/files/zzz-default-settings      ## 再初始默认基础上修改为：OpenWrtx86
+# 修改主机名（不能纯数字或者中文）在机型脚本中修改，这里不修改。
+# sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate                                                                    ## 初始默认的“OpenWrt” 修改为：G-DOCK
+# sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrtx86'' package/lean/default-settings/files/zzz-default-settings      ## 生成默认的基础上添加为：OpenWrtx86
 
 # 修改固件版本号 添加代码：LEDE build $(TZ=UTC-8 date "+%Y.%m.%d") 显示范例：LEDE build 2021.02.08 @ OpenWrt        说明：【LEDE=作者 + build=建造 + （UTC-8=字符编码 + date=时间格式）】
 sed -i "s/OpenWrt /LEDE build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-# 修改 默认主题
+# 修改 默认主题（添加luci-theme-argon主题时，会自动设置为默认）
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap   # 命令的结果是删除代码：set luci.main.mediaurlbase=/luci-static/bootstrap
 # sed -i 's/ +luci-theme-bootstrap//g' feeds/luci/collections/luci/Makefile                                                                             # 取掉默认主题
@@ -41,25 +41,77 @@ sed -i "s/OpenWrt /LEDE build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package
 # 更改 Argon 主题背景
 cp -f $lede_path/build/DIY/img/bg1.jpg package/otherapp/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
-
-
 # echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n\t\tset system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate                 ## 把 UTC 时区改为：CST-8  并换行添加：set system.@system[-1].zonename='Asia/Shanghai'（ \t\tset = 用于对齐新插入的行）
 
-# 修改输出固件名称（参考：Lede-20240726-openwrt）
+# 修改内核版本
+# grep 'KERNEL_PATCHVER:=' target/linux/x86/Makefile                                                            ## 查看当前内核版本
+# sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile                              ## 修改5.15=内核版本
+
+# 修改输出固件名称（案例：Lede-20240726-openwrt）
 # sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=Lede-$(shell date +%Y-%m-%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
-# 添加自定义软件包
+# 添加自定义软件包（Luci插件）
 # echo '
 # CONFIG_PACKAGE_luci-app-mosdns=y
 # CONFIG_PACKAGE_luci-app-adguardhome=y
 # CONFIG_PACKAGE_luci-app-openclash=y
 # ' >> .config
 
-# 修改内核版本
-#sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
 
 
+# ----------------------我是分界线，以下是非必须部分--------------------------------------------------------------------------------------------
+# feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/index.lua                # 直接编辑左侧菜单选项（不推荐）
+# feeds/luci/modules/luci-base/po/zh-cn/base.po                                           # 添加多语言翻译（推荐修改 base.po 文件）
+
+# 修改 base.po 翻译文件
+sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '/msgid "VPN"/{n;s/.*/msgstr "虚拟专用网络"/}' feeds/luci/modules/luci-base/po/zh-cn/base.po   ## 修改 VPN=虚拟专用网络
+
+
+# 添加 base.po 翻译文件
+# echo -e '\nmsgid "VPN"\nmsgstr "虚拟专用网络"' >> feeds/luci/modules/luci-base/po/zh-cn/base.po       ## 添加 VPN=虚拟专用网络
+
+
+# 修改已有插件名
+# sed -i 's/TTYD 终端/命令窗/g' feeds/luci/applications/luci-app-ttyd/po/zh-cn/terminal.po
+# sed -i 's/"Turbo ACC 网络加速"/"Turbo ACC 网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po            # 把默认 Turbo ACC 网络加速  修改为：网络加速
+sed -i 's/"KMS 服务器"/"KMS激活"/g' feeds/luci/applications/luci-app-vlmcsd/po/zh-cn/vlmcsd.po
+# sed -i 's/"Web 管理"/"Web管理"/g' feeds/luci/applications/luci-app-webadmin/po/zh-cn/webadmin.po
+
+
+# 移动插件菜单项
+#sed -i 's/{"admin", "vpn"/{"admin", "nas"/g' feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua      # 移动 “luci-app-zerotier” 从 vpn 移动至 nas 菜单中； （确保关联的 luasrc/controller/admin/index.lua 文件中存在）；
+
+
+
+
+
+
+
+
+
+
+
+
+
+# sed -i 's/cbi("qbittorrent"), _("qBittorrent"), 20/cbi("qbittorrent"), _("BT下载"), 20/g' package/otherapp/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua    # 把第二个 qbittorrent   修改为：BT下载
+# sed -i 's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
+# sed -i 's/cbi("qbittorrent"),_("qBittorrent")/cbi("qbittorrent"),_("BT下载")/g' package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
+# sed -i 's/"aMule设置"/"电驴下载"/g' package/lean/luci-app-amule/po/zh-cn/amule.po
+# sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-amule/po/zh-cn/amule.po
+# sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
+# sed -i 's/"实时流量监测"/"流量"/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
+# sed -i 's/"USB 打印服务器"/"打印服务"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
+# sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
+# sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
+
+
+
+# ----------------------无效的代码--------------------------------------------------------------------------------------------------------------
+
+# 添加温度显示（在703行  or "1"%>   后面添加代码）无效果
+# sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 
 # git clone https://github.com/openwrt-xiaomi/luci-app-cpufreq package/otherapp/luci-app-cpufreq              # CPU性能调节（适用arm处理器）
@@ -67,69 +119,20 @@ sed -i "s/'UTC'/'CST-8'\n\t\tset system.@system[-1].zonename='Asia\/Shanghai'/g"
 # sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua                    # 把默认 services 修改为：system
 
 
-
-
-# 添加温度显示(By YYiiEt)   # 在703行  or "1"%>   后面添加代码；
-# sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-
-
-
-# 主题
-# https://github.com/garypang13/luci-theme-edge        # Edge 带背景音乐
-
-
-
-
-
-
 # x86 型号只显示 CPU 型号
 #sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
+
 
 # 修改本地时间格式
 #sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
+
 #避免出现下面的警告 提前将mosdns_neo 改为mosdns （2022.08.13目前新版源码貌似已修复此警告）
 #sed -i 's/mosdns.*neo/mosdns/g' feeds/kenzo/luci-app-mosdns/Makefile
 
-# ----------------------无效的代码------------------------
+
 # 修改版本号
 # sed -i 's/V2020/V$(date "+%Y.%m.%d")/g' package/lean/default-settings/files/zzz-default-settings                  # 修改版本号 将文件中所有的 V2020 替换为当前日期，格式为 YYYY.MM.DD。
 # sed -i 's/V2020/V2024/g' package/base-files/files/etc/banner
-
-
-
-# ----------------------我是分界线------------------------
-# --------------------以下是非必须部分--------------------
-
-
-
-
-
-
-# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
-sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
-
-sed -i 's/"KMS 服务器"/"KMS激活"/g' feeds/luci/applications/luci-app-vlmcsd/po/zh-cn/vlmcsd.po
-# sed -i 's/"Turbo ACC 网络加速"/"Turbo ACC 网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po   # 把默认 Turbo ACC 网络加速  修改为：网络加速
-# sed -i 's/cbi("qbittorrent"), _("qBittorrent"), 20/cbi("qbittorrent"), _("BT下载"), 20/g' package/otherapp/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua    # 把第二个 qbittorrent   修改为：BT下载
-
-
-
-# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
-# sed -i 's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
-# sed -i 's/cbi("qbittorrent"),_("qBittorrent")/cbi("qbittorrent"),_("BT下载")/g' package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
-# sed -i 's/"aMule设置"/"电驴下载"/g' package/lean/luci-app-amule/po/zh-cn/amule.po
-# sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-amule/po/zh-cn/amule.po
-# sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
-# sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
-# sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
-# sed -i 's/"实时流量监测"/"流量"/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
-# sed -i 's/"KMS 服务器"/"KMS激活"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
-# sed -i 's/"USB 打印服务器"/"打印服务"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
-# sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
-# sed -i 's/TTYD 终端/命令窗/g' feeds/luci/transplant/luci-app-ttyd/po/zh-cn/terminal.po
-# sed -i 's/"Web 管理"/"Web管理"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
-# sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
-# sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 
 
